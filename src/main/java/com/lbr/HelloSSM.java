cc15ec49b1149ffb50453d3b3e1b7569e2a5811d
liuchj <liuchj0112@126.com>
2018-11-09 22:47:14 +0800

    SSM-idea-github

---
+++ src/main/java/com/lbr/HelloSSM.java
@@ -0,0 +1,12 @@
+package com.lbr;
+
+/**
+ * Created by liuchj on 2018/11/09.
+ */
+public class HelloSSM {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld ssm idea github...");
+    }
+    
+}
